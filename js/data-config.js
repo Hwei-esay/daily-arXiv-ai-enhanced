95a7d841c3ff23c0f0fede4597e992906c834f42
huangpipip <69191498+huangpipip@users.noreply.github.com>
2026-03-08 02:18:45 +0800

    Update data-config.js

--- js/data-config.js
+++ js/data-config.js
@@ -12,7 +12,7 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'dw-dengwei',
+    repoOwner: 'huangpipip',
 
     /**
      * GitHub repository name
